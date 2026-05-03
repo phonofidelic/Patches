2b221abf4bdc28ae62f49ffe8a059a4dee4a21fd
Christopher Clemons <chris.clemons85@gmail.com>
2026-05-03 17:06:03 +0200

    Extract DI registrations into ServiceCollectionExtensions

---
+++ Patches.CLI/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,50 @@
+using Microsoft.Extensions.DependencyInjection;
+using Patches.Application.Contracts;
+using Patches.Application.Handlers;
+using Patches.CLI.App;
+using Patches.Domain.Entities;
+using Patches.Infrastructure.ModulargridApi;
+using Patches.Infrastructure.Repositories;
+using Patches.Services;
+using Patches.Shared.Commands;
+using Patches.Shared.Queries;
+using Spectre.Console;
+
+namespace Patches.CLI.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddRepositories(this ServiceCollection services)
+    {
+        services.AddScoped<IRepository<Module, Guid>, ModuleRepository>();
+        services.AddScoped<IRepository<Vendor, Guid>, VendorRepository>();
+        services.AddScoped<IRepository<ConnectionPoint, int>, ConnectionPointRepository>();
+        services.AddScoped<IConnectionRepository, ConnectionRepository>();
+        services.AddScoped<IRepository<Patch, int>, PatchRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+    }
+    public static void AddHandlers(this ServiceCollection services)
+    {
+        services.AddScoped<IHandler<AddModuleCommand, AddModuleResult>, AddModuleHandler>();
+        services.AddScoped<IHandler<ListModulesQuery, ListModulesQueryResult>, ListModulesHandler>();
+        services.AddScoped<IHandler<ImportModulesFromJsonCommand, ImportModulesFromJsonResult>, ImportModulesFromJsonHandler>();
+        services.AddScoped<IHandler<LoadPatchMatrixQuery, LoadPatchMatrixResult>, LoadPatchMatrixHandler>();
+        services.AddScoped<IHandler<AddConnectionCommand, AddConnectionResult>, AddConnectionHandler>();
+        services.AddScoped<IHandler<DeleteConnectionCommand, DeleteConnectionResult>, DeleteConnectionHandler>();
+        services.AddScoped<IHandler<ListPatchesQuery, ListPatchesQueryResult>, ListPatchesQueryHandler>();
+    }
+
+    public static void AddConsoleApp(this ServiceCollection services)
+    {
+        services.AddScoped<IConsoleUIService, ConsoleUIService>();
+        services.AddScoped<IModulargridApiClient, ModulargridApiClient>();
+        services.AddScoped((sp) => AnsiConsole.Create(new AnsiConsoleSettings()));
+        services.AddScoped<HelpScreen>();
+        services.AddScoped<ModulesList>();
+        services.AddScoped<AddModuleForm>();
+        services.AddScoped<ImportModulesFromJsonForm>();
+        services.AddScoped<PatchMatrixScreen>();
+        services.AddScoped<LoadPatchScreen>();
+        services.AddScoped<PatchesCLI>();      
+    }
+}
--- Patches.CLI/Program.cs
+++ Patches.CLI/Program.cs
@@ -1,18 +1,10 @@
 using AutoMapper;
 using Microsoft.Extensions.DependencyInjection;
-using Patches.Application.Contracts;
-using Patches.Services;
 using Patches.CLI;
-using Patches.Application.Handlers;
 using Patches.Infrastructure.Data;
-using Patches.Shared.Commands;
 using Microsoft.EntityFrameworkCore;
-using Patches.Infrastructure.Repositories;
 using Patches.Domain.Entities;
-using Patches.Shared.Queries;
-using Patches.Infrastructure.ModulargridApi;
-using Spectre.Console;
-using Patches.CLI.App;
+using Patches.CLI.Extensions;
 
 var services = new ServiceCollection();
 
@@ -23,35 +15,11 @@ services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlite($"Data Source={dbPath}");
 });
-
 services.AddLogging();
-services.AddScoped<IRepository<Module, Guid>, ModuleRepository>();
-services.AddScoped<IRepository<Vendor, Guid>, VendorRepository>();
-services.AddScoped<IRepository<ConnectionPoint, int>, ConnectionPointRepository>();
-services.AddScoped<IConnectionRepository, ConnectionRepository>();
-services.AddScoped<IRepository<Patch, int>, PatchRepository>();
-services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-services.AddScoped<IHandler<AddModuleCommand, AddModuleResult>, AddModuleHandler>();
-services.AddScoped<IHandler<ListModulesQuery, ListModulesQueryResult>, ListModulesHandler>();
-services.AddScoped<IHandler<ImportModulesFromJsonCommand, ImportModulesFromJsonResult>, ImportModulesFromJsonHandler>();
-services.AddScoped<IHandler<LoadPatchMatrixQuery, LoadPatchMatrixResult>, LoadPatchMatrixHandler>();
-services.AddScoped<IHandler<AddConnectionCommand, AddConnectionResult>, AddConnectionHandler>();
-services.AddScoped<IHandler<DeleteConnectionCommand, DeleteConnectionResult>, DeleteConnectionHandler>();
-services.AddScoped<IHandler<ListPatchesQuery, ListPatchesQueryResult>, ListPatchesQueryHandler>();
-
-services.AddSingleton((sp) => AnsiConsole.Create(new AnsiConsoleSettings()));
-services.AddSingleton<IConsoleUIService, ConsoleUIService>();
-services.AddSingleton<HelpScreen>();
-services.AddScoped<ModulesList>();
-services.AddScoped<AddModuleForm>();
-services.AddScoped<ImportModulesFromJsonForm>();
-services.AddScoped<PatchMatrixScreen>();
-services.AddScoped<LoadPatchScreen>();
-services.AddSingleton<PatchesCLI>();
-
+services.AddRepositories();
+services.AddHandlers();
+services.AddConsoleApp();
 services.AddAutoMapper(config => config.AddProfile<MapperProfile>());
-services.AddScoped<IModulargridApiClient, ModulargridApiClient>();
 
 var serviceProvider = services.BuildServiceProvider();
 
